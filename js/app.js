613305bbf301b4ea3208a5b5f80a5bfa84baf0a8
ZachMoore <Get3Moore@gmail.com>
2018-02-13 18:07:04 -0500

    Clean up code, remove white space, add semi-colons, add comments.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-//random whole number between 0 and x: "Math.floor(Math.random() * x);"
-
-
-//Variables and constants
+//Main ariables and constants
 
 const gameContainer = document.querySelector("#gameContainer");
 const victoryModal = document.querySelector("#victory");
@@ -12,7 +9,7 @@ const movesMade = document.querySelector("#movesMade");
 const movesMadeEnd = document.querySelector("#movesMadeEnd");
 const headText = document.querySelector("h1");
 const starsContainer = document.querySelector("#stars");
-const starsContainerEnd = document.querySelector("#starsEnd")
+const starsContainerEnd = document.querySelector("#starsEnd");
 const timeTaken = document.querySelector("#timeTaken");
 const content = {
     gameHidden: "game-container content-hidden",
@@ -25,42 +22,41 @@ let selectionCounter = 1;
 let movesMadeCounter = -1;
 let matchProgress = 0;
 let endTime;
-//eventlisteners
 
+//this resets the match when pressing the little undo icon
 reset.addEventListener("click", function () {
     resetGame();
 });
-
+//this listens for clicks either on the start button or if the game has already started and the player is selecting a card.
 gameCanvas.addEventListener("click", function (event) {
     if (event.target.nodeName === "H2") {
         resetGame();
     } else if (event.target.nodeName === "DIV" || "I") {
         selectCard(event);
-    }
+    };
 });
+//this restarts the game if the end restart button is pressed as well as unhides the main area and then hides the modal.
 document.querySelector("#restart").addEventListener("click", function () {
     resetGame();
     gameContainer.className = content.gameShown;
     victoryModal.className = content.modalHidden;
-    console.log("Game was shown. Modal was hidden.");
-})
+});
 
 //Functions
 
 function resetGame() {
-    console.log("resetGame function was called");
+    //Creates cards
     let fragment = document.createDocumentFragment();
-    for (let e = 1; e <= 2; e++) {
-        console.log(e);
+    for (let e = 1; e <= 2; e++) { //this loops twice to create two of each card
         for (let i = 1; i <= 8; i++) {
-            let y = 0
-            y += i
+            //this y and if statement let me count to 16 gradually 1 number per every loop allowing me to assign unique id names while also creating two cards of the same kind
+            let y = 0;
+            y += i;
             if (e === 2) {
-                y = 8 + i
+                y = 8 + i;
             }
             let newDiv = document.createElement("div");
             let newIcon = document.createElement("i");
-            console.log(`loop number ${y}`);
             newDiv.className = "card flex-container";
             newDiv.id = `card${y}`;
             newIcon.className = `fa fa-custom fa-${icons[i]}`;
@@ -68,49 +64,53 @@ function resetGame() {
             newDiv.appendChild(newIcon);
             fragment.appendChild(newDiv);
 
-        }
-    }
+        };
+    };
+    //erases the contents of the game canvas and appends all of the cards created above into it
     gameCanvas.innerHTML = "";
     gameCanvas.appendChild(fragment);
 
     //Randomize Cards
     for (let i = 1; i <= 16; i++) {
+        //generates 2 random number between 1 and 16 and applies them to seperate variables
         let randomNumber = Math.floor(Math.random() * 16);
         if (randomNumber === 0) {
             randomNumber = 1;
-        }
+        };
         let randomNumber2 = Math.floor(Math.random() * 16);
         if (randomNumber === 0) {
             randomNumber = 1;
-        }
-        console.log(randomNumber);
+        };
+        //using the first random number, select a card with id of card[randomNumber], then randomly select another card. Take these two cards and switch them around. loop once per card in the game (which is 16). This may not be the most effective way to do it, but it seems fast and it gives a good randomization.
         let card = document.querySelector(`#card${randomNumber}`);
         let card2 = document.querySelector(`#card${randomNumber2}`);
         gameCanvas.insertBefore(card, card2);
     }
+    //resets variables tracking how many moves they have made, and how many matches have been made, then update these changes.
     movesMadeCounter = -1;
     matchProgress = 0;
     updateScore();
+    //this loops through and creates the stars for the beginning screen, and erases the contents before appending. This resets how many stars a player has.
     let starFragment = document.createDocumentFragment();
     for (let i = 1; i <= 3; i++) {
         let newStar = document.createElement("i");
-        newStar.className = "fa fa-star"
+        newStar.className = "fa fa-star";
         newStar.id = `star${i}`;
         starFragment.appendChild(newStar);
-    }
+    };
     starsContainer.innerHTML = "";
     starsContainer.appendChild(starFragment);
-    //reset end stars
+    //this does the exact same thing except for the end modal screen instead.
     let starEndFragment = document.createDocumentFragment();
     for (let i = 4; i <= 6; i++) {
         let newStar = document.createElement("i");
-        newStar.className = "fa fa-star"
+        newStar.className = "fa fa-star";
         newStar.id = `star${i}`;
         starEndFragment.appendChild(newStar);
-    }
+    };
     starsContainerEnd.innerHTML = "";
     starsContainerEnd.appendChild(starEndFragment);
-    console.log("resetGame function completed");
+    //once all this ran becasue a player has either pressed the start button, reset, or restart, their start time begins.
     startTime = performance.now();
 };
 
@@ -121,31 +121,37 @@ let selectionTwoClasses;
 const cardDefaultClasses = " card flex-container ";
 
 function selectCard(event) {
+    //this checks to make sure the player is not clicking on the card container (gameCanvas), then check to see if this is this first selection
     if (event.target.nodeName === "DIV" && selectionCounter === 1) {
+        //sets the event target to variables for easier use.
         selectionOne = event.target
         selectionOneClasses = event.target.firstChild.className;
+        //this ensures that a class (e.g "fa-flipped") has not been added already. Essentially checking if a card is already flipped
         if (selectionOneClasses.length < 25) {
+            //selection is counted so I know the next click will be their second choice. also then flips the card over
             selectionCounter++;
             selectionOne.firstChild.className = selectionOneClasses + " fa-flipped";
             selectionOne.className = cardDefaultClasses + " card-flipped";
-            console.log(`clicked on div with child class contents of ${selectionOne.firstChild.className} as your first selection`);
-        }
-    } else if (event.target.nodeName === "DIV" && selectionCounter === 2 && event.target.id !== selectionOne.id) {
+        };
+    } //if this is not the gameCanvas and they are on their second selection and they arent selecting the same card as before, continue.
+    else if (event.target.nodeName === "DIV" && selectionCounter === 2 && event.target.id !== selectionOne.id) {
         selectionCounter++;
         selectionTwo = event.target;
         selectionTwoClasses = event.target.firstChild.className;
+        //this compares the two selected i element classes against each other to see if they match.
         if (selectionOneClasses === selectionTwoClasses) {
-            console.log("you made a match");
+            //if they match, the match progress goes up indicating they have made x amount of pairs so far. selectionCounter gets reset and the card is turned green and the icon is made visible.
             matchProgress++
-            console.log(matchProgress);
             selectionCounter = 1;
             selectionTwo.firstChild.className = selectionTwoClasses + " fa-flipped";
             selectionTwo.className = cardDefaultClasses + " card-flipped";
         } else {
+            //if its not a match, reset the counter, change both cards over with a red background.
             selectionCounter = 1;
             selectionTwo.firstChild.className = selectionTwoClasses + " fa-flipped";
             selectionOne.className = cardDefaultClasses + " card-flipped card-incorrect";
             selectionTwo.className = cardDefaultClasses + " card-flipped card-incorrect";
+            //wait for a small amount of time to give a glimpse, then reset things back to normal defaults.
             setTimeout(function () {
                 selectionOne.firstChild.className = selectionOneClasses;
                 selectionOne.className = cardDefaultClasses;
@@ -153,51 +159,50 @@ function selectCard(event) {
                 selectionTwo.className = cardDefaultClasses;
             }, 250);
         }
+        //updates the text and stars on the screens as well as checks to see if they won.
         updateScore();
         checkForWin();
     }
 }
-//if (event.target.nodeName === "I") {
-//    event.target = event.target.parentElement;
-//}
 
 function updateScore() {
+    //counts how many guess/moves have been made so far and updates the text in index.html
     movesMadeCounter += 1;
     movesMade.textContent = movesMadeCounter;
     movesMadeEnd.textContent = movesMadeCounter;
+    //if they have made 13 guesses, remove a star, if they get to 15, remove a star, etc.
     if (movesMadeCounter === 13) {
         let star1 = document.querySelector("#star1");
         let star4 = document.querySelector("#star4");
-        console.log("if statement")
         star1.parentElement.removeChild(star1);
         star4.parentElement.removeChild(star4);
     }
     if (movesMadeCounter === 15) {
         let star2 = document.querySelector("#star2");
         let star5 = document.querySelector("#star5");
-        console.log("if statement")
         star2.parentElement.removeChild(star2);
         star5.parentElement.removeChild(star5);
     }
     if (movesMadeCounter === 19) {
         let star3 = document.querySelector("#star3");
         let star6 = document.querySelector("#star6");
-        console.log("if statement")
         star3.parentElement.removeChild(star3);
         star6.parentElement.removeChild(star6);
     }
 }
 
 function checkForWin() {
+    //if the player has made 8 matches, which is the total number possible, then the heading changes to say congrats, their time gets stopped.
     if (matchProgress === 8) {
         headText.textContent = "Congrats!";
         endTime = performance.now();
         setTimeout(function () {
+            //then it waits for one second so they can see they won, then calculate the time taken, round up, and set it.
             timeTaken.textContent = Math.round(((endTime - startTime) / 1000))
+            //display the end modal, and hide the main area.
             gameContainer.className = content.gameHidden;
             victoryModal.className = content.modalShown;
-            console.log("Game was hidden. Modal was shown.");
             headText.textContent = "Memory Match!";
         }, 1000);
     };
-}
+};
