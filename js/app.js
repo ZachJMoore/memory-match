e563e878a155e0682650b25f03bf61301925a0dd
ZachMoore <Get3Moore@gmail.com>
2018-02-13 14:07:41 -0500

    Add mostly working javascript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,172 @@
 //random whole number between 0 and x: "Math.floor(Math.random() * x);"
-document.querySelector("#testWin").addEventListener('click', function(){
-    document.querySelector("#victory").style.visibility = "visible";
-    document.querySelector("main").style.visibility = "hidden";
-    document.querySelector("header").style.visibility = "hidden";
-    console.log("element was clicked");
+
+
+//Variables and constants
+
+const gameContainer = document.querySelector("#gameContainer");
+const victoryModal = document.querySelector("#victory");
+const gameCanvas = document.querySelector("#gameCanvas");
+const startGame = document.querySelector("#startGame");
+const reset = document.querySelector("#reset");
+const movesMade = document.querySelector("#movesMade")
+const headText = document.querySelector("h1");
+const starsContainer = document.querySelector("#stars");
+let star1 = document.querySelector("#star1");
+let star2 = document.querySelector("#star2");
+let star3 = document.querySelector("#star3");
+const content = {
+    gameHidden: "game-container content-hidden",
+    gameShown: "game-container",
+    modalHidden: "victory flex-container",
+    modalShown: "victory flex-container content-visible"
+};
+const icons = ["code", "linux", "space-shuttle", "ambulance", "envira", "balance-scale", "paper-plane", "anchor", "code"];
+let selectionCounter = 1;
+let movesMadeCounter = 0;
+let matchProgress = 0;
+//eventlisteners
+
+reset.addEventListener("click", function () {
+    makeMatches();
+});
+
+gameCanvas.addEventListener("click", function (event) {
+    if (event.target.nodeName === "H2") {
+        makeMatches();
+    } else if (event.target.nodeName === "DIV" || "I"){
+        selectCard(event);
+    } 
+});
+document.querySelector("#testWin").addEventListener("click", function () {
+    gameContainer.className = content.gameHidden;
+    victoryModal.className = content.modalShown;
+    console.log("Game was hidden. Modal was shown.");
+});
+document.querySelector("#restart").addEventListener("click", function () {
+    makeMatches();
+    gameContainer.className = content.gameShown;
+    victoryModal.className = content.modalHidden;
+    console.log("Game was shown. Modal was hidden.");
 })
-document.querySelector("#restart").addEventListener('click', function(){
-    document.querySelector("#victory").style.visibility = "hidden";
-    document.querySelector("main").style.visibility = "visible";
-    document.querySelector("header").style.visibility = "visible";
-    console.log("element was clicked");
-})
+
+//Functions
+
+function makeMatches() {
+    console.log("makeMatches function was called");
+    let fragment = document.createDocumentFragment();
+    for (let e = 1; e <= 2; e++) {
+        console.log(e);
+        for (let i = 1; i <= 8; i++) {
+            let y = 0
+            y += i
+            if (e === 2) {
+                y = 8 + i
+            }
+            let newDiv = document.createElement("div");
+            let newIcon = document.createElement("i");
+            console.log(`loop number ${y}`);
+            newDiv.className = "card flex-container";
+            newDiv.id = `card${y}`;
+            newIcon.className = `fa fa-custom fa-${icons[i]}`;
+            newIcon.id = `cardContent${y}`;
+            newDiv.appendChild(newIcon);
+            fragment.appendChild(newDiv);
+
+        }
+    }
+    gameCanvas.innerHTML = "";
+    gameCanvas.appendChild(fragment);
+
+    //Randomize Cards
+    for (let i = 1; i <= 16; i++) {
+        let randomNumber = Math.floor(Math.random() * 16);
+        if (randomNumber === 0) {
+            randomNumber = 1;
+        }
+        let randomNumber2 = Math.floor(Math.random() * 16);
+        if (randomNumber === 0) {
+            randomNumber = 1;
+        }
+        console.log(randomNumber);
+        let card = document.querySelector(`#card${randomNumber}`);
+        let card2 = document.querySelector(`#card${randomNumber2}`);
+        gameCanvas.insertBefore(card, card2);
+    }
+    movesMadeCounter = -1;
+    updateScore();
+    console.log("makeMatches function completed");
+};
+
+let selectionOne;
+let selectionTwo;
+let selectionOneClasses;
+let selectionTwoClasses;
+const cardDefaultClasses = " card flex-container ";
+
+function selectCard(event){
+    if (event.target.nodeName === "DIV" && selectionCounter === 1) {
+        selectionCounter++;
+        selectionOne = event.target
+        selectionOneClasses = event.target.firstChild.className;
+        selectionOne.firstChild.className = selectionOneClasses + " fa-flipped";
+        selectionOne.className = cardDefaultClasses + " card-flipped";
+        console.log(`clicked on div with child class contents of ${selectionOne.firstChild.className} as your first selection`);
+    } else if (event.target.nodeName === "DIV" && selectionCounter === 2 && event.target.id !== selectionOne.id) {
+        selectionCounter++;
+        selectionTwo = event.target;
+        selectionTwoClasses = event.target.firstChild.className;
+        if (selectionOneClasses === selectionTwoClasses){
+            console.log("you made a match");
+            matchProgress++
+            console.log(matchProgress);
+            selectionCounter = 1;
+            selectionTwo.firstChild.className = selectionTwoClasses + " fa-flipped";
+            selectionTwo.className = cardDefaultClasses + " card-flipped";
+        } else {
+            selectionCounter = 1;
+            selectionTwo.firstChild.className = selectionTwoClasses + " fa-flipped";
+            selectionOne.className = cardDefaultClasses + " card-flipped card-incorrect";
+            selectionTwo.className = cardDefaultClasses + " card-flipped card-incorrect";
+            setTimeout(function(){
+                selectionOne.firstChild.className = selectionOneClasses;
+                selectionOne.className = cardDefaultClasses;
+                selectionTwo.firstChild.className = selectionTwoClasses;
+                selectionTwo.className = cardDefaultClasses;
+            }, 250);
+        }
+        updateScore();
+        checkForWin();
+    }
+}
+//if (event.target.nodeName === "I") {
+//    event.target = event.target.parentElement;
+//}
+
+function updateScore(){
+    movesMadeCounter += 1;
+    movesMade.textContent = movesMadeCounter;
+    if (movesMadeCounter === 12){
+        console.log("if statement")
+        star1.parentElement.removeChild(star1);
+    }
+    if (movesMadeCounter === 15){
+        console.log("if statement")
+        star2.parentElement.removeChild(star2);
+    }
+    if (movesMadeCounter === 18){
+        console.log("if statement")
+        star3.parentElement.removeChild(star3);
+    }
+}
+function checkForWin(){
+    if (matchProgress === 8){
+        let cards = document.querySelectorAll(".card")
+        headText.textContent = "Congrats!";
+        setTimeout(function(){
+            gameContainer.className = content.gameHidden;
+            victoryModal.className = content.modalShown;
+            console.log("Game was hidden. Modal was shown.");
+            headText.textContent = "Memory Match!";
+        }, 1000);
+    };
+}
